831e76ad043a6a158c68493b6d992952b819c0d9
Eric Fossoul <weloveworks@gmail.com>
2019-11-07 14:42:01 +0100

    "update"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
+import 'bootstrap';
+console.log('Hello World from Webpacker')
+
+
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -15,4 +19,3 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
